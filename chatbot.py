d40ecffb2df4c33869b414f4181b9522cd7f1a81
aberger1999 <aberger1999@gmail.com>
2022-03-27 14:24:07 -0400

    try out the chatbot!

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 import nltk
-nltk.download('punkt')
+##nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
@@ -13,57 +13,50 @@ import pickle
 with open("intents.json") as file:
     data = json.load(file)
 
-try:
-    with open("data.pickle", "rb") as f:
-        words, labels, training, output = pickle.load(f)
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
+words = []
+labels = []
+docs_x = []
+docs_y = []
 
-    ##getting the intents
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-                wrds = nltk.word_tokenize(pattern)
-                words.extend(wrds)
-                docs_x.append(wrds)
-                docs_y.append(intent["tag"])
+##getting the intents
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
 
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
 
-    labels = sorted(labels)
+labels = sorted(labels)
 
-    training = []
-    output = []
+training = []
+output = []
 
-    out_empty = [0 for _ in range(len(labels))]
+out_empty = [0 for _ in range(len(labels))]
 
-    for x, doc in enumerate(docs_x):
-        bag = []
+for x, doc in enumerate(docs_x):
+    bag = []
 
-        wrds = [stemmer.stem(w) for w in doc]
+    wrds = [stemmer.stem(w) for w in doc]
 
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
 
-        training.append(bag)
-        output.append(output_row)
+    training.append(bag)
+    output.append(output_row)
 
-    training = numpy.array(training)
-    output = numpy.array(output)
-
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words, labels, training, output), f)
+training = numpy.array(training)
+output = numpy.array(output)
 
 tensorflow.compat.v1.reset_default_graph()
 
@@ -75,11 +68,9 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save("model.tflearn")
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
@@ -109,5 +100,6 @@ def chat():
             if tg['tag'] == tag:
                 responses = tg['responses']
         print(random.choice(responses))
+
 
 chat()
