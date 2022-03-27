49775fb9ebc85ee3a65c7bbe6537319e2e2e0782
JengaMasterG <grsruggirello25@gmail.com>
2022-03-27 10:03:53 -0400

    added chatbot page as main project page

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return render_template("home.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("chatbot.html")
